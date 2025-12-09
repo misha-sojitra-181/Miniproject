ed9ee774db710e94432f1255bbb1a31a5ff69127
misha-sojitra-181 <mishasojitra@gmail.com>
2025-12-09 18:28:06 +0530

    Update Userdashboard.js

--- mainuser/src/components/Userdashboard.js
+++ mainuser/src/components/Userdashboard.js
@@ -14,65 +14,32 @@ const menu = [
 function Userdashboard() {
   return (
     <div className="bod">
-    <div style={{ display: "flex" }}>
-      <div
-        className="sidebar"
-        style={{
-          width: "250px",
-          minHeight: "100vh",
-          backgroundColor: "#f8f9fa",
-          borderRight: "1px solid #ddd",
-          padding: "20px",
-          overflowY: "auto",
-          overflowX: "hidden",
-          position: "fixed",
-        }}
-      >
-        <div className="logo" style={{ textAlign: "center", marginBottom: "20px" }}>
-          <img
-            src={logo}
-            alt="Logo"
-            width="100"
-            height="100"
-            style={{ borderRadius: "50%" }}
-          />
-          <h4 style={{ fontSize: "22px", color: "#16009e", marginTop: "10px" }}>
-            {projectName}
-          </h4>
+      <div className="sidebar">
+        <div className="logo">
+          <img src={logo} width="100" height="100" alt="logo" />
+          <h4>{projectName}</h4>
         </div>
 
-      <a href="/Userprofile" className="btn btn-outline-primary mb-3"style={{ width: "100%", textAlign: "center" }} >
-  <i className="fa-solid fa-user"></i> Profile
-</a>
+        <a href="/Userprofile" className="btn btn-outline-primary mb-3" style={{ width: "100%", textAlign: "center" }}>
+          <i className="fa-solid fa-user"></i> Profile
+        </a>
 
-
-        <nav className="nav flex-column ">
+        <nav className="nav flex-column">
           {menu.map((item, index) => (
-            <a
-              key={index}
-              href={item.link}
-              className="nav-link"
-              style={{
-                color: "#05337e",
-                margin: "5px 0",
-                borderRadius: "8px",
-                padding: "10px",
-                boxShadow: "2px 2px 4px #ddd",
-              }}
-            >
+            <a key={index} href={item.link} className="nav-link">
               <i className={item.icon}></i> {item.name}
             </a>
           ))}
         </nav>
       </div>
 
-      <div className="content" style={{ flexGrow: 1, padding: "20px", marginLeft: "250px" }}>
-        
+      <div className="content">
+        {/* Your page content */}
       </div>
-    </div>
     </div>
   );
 }
+
 
 export default Userdashboard;
 
